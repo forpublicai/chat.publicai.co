0593a565561a26b5a5b3b980dba94bbbaa00fd40
sean <junglecatsw@gmail.com>
2026-07-27 17:02:42 +0100

    chore: remove outdated developer environment setup and configuration files

--- developer/init-db.sql
+++
@@ -1,7 +0,0 @@
--- Create LiteLLM user and database
-CREATE USER llmproxy WITH PASSWORD 'password';
-CREATE DATABASE litellm OWNER llmproxy;
-
--- Create OpenWebUI user and database
-CREATE USER openwebui WITH PASSWORD 'password';
-CREATE DATABASE openwebui OWNER openwebui;
